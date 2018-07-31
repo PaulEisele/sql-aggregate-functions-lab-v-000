81819d5abca65f2c2359b3ca0af4b0d9b21bbfcb
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-31 01:46:29 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,9 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(gpa) FROM students"
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVE(gpa) FROM students WHERE grade=9"
+  "SELECT AVE(gpa) FROM students WHERE grade = 9"
 end
